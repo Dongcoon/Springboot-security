8e1ace30956ffbc1190e68779ef009b6711755a9
Kang <gongan1556@naver.com>
2023-02-21 16:26:08 +0900

    Spring Security 3강 시큐리티 회원가입

--- src/main/java/com/cos/security1/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/security1/controller/IndexController.java
@@ -1,11 +1,23 @@
 package com.cos.security1.security1.controller;
 
+import com.cos.security1.security1.model.User;
+import com.cos.security1.security1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //View 리턴
 public class IndexController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
 
     // localhost:8080/
     // localhost:8080
@@ -30,19 +42,26 @@ public class IndexController {
         return "manager";
     }
 
-    @GetMapping("/login")
-    public @ResponseBody String login(){
-        return "login";
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "loginForm";
     }
 
-    @GetMapping("/join")
-    public @ResponseBody String join(){
-        return "join" ;
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "joinForm";
     }
 
-    @GetMapping("/joinProc")
-    public @ResponseBody String joinProc(){
-        return "회원가입 완료됨" ;
+    @PostMapping("/join")
+    public String join(User user){
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        //회원가입 잘됨. 비밀번호 1234 => 시큐리티로 로그인을 할 수 없음. 패스워드가 암호화가 안되어서
+        userRepository.save(user);
+        return "redirect:/loginForm";
     }
 
 }
