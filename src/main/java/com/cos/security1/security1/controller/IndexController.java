90cf5b436e1022f1f4f1013341d1b7ae1c12ee41
Kang <gongan1556@naver.com>
2023-02-21 15:34:36 +0900

    Spring Security 2강 시큐리티 설정(controller, config 설정)

--- src/main/java/com/cos/security1/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.security1.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //View 리턴
 public class IndexController {
@@ -14,6 +15,34 @@ public class IndexController {
         // 뷰리졸버 설정: template(prefix), .mustache(suffix) 생략가능
         return "index"; // src/main/resouces/templates/index.mustache
     }
+    @GetMapping("/user")
+    public @ResponseBody String user(){
+        return "user" ;
+    }
 
+    @GetMapping("/admin")
+    public @ResponseBody String admin(){
+        return "admin" ;
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public @ResponseBody String login(){
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join(){
+        return "join" ;
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc(){
+        return "회원가입 완료됨" ;
+    }
 
 }
